56e805b96038337858d143c8c4330a3cec22d490
MateoBeMo <mateobernal19@gmail.com>
2017-12-07 14:20:08 +0100

    nuevo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ChartsModule } from 'ng2-charts';
+import { GraficosComponent } from './graficos/graficos.component';
+import { PrincipalComponent } from './principal/principal.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraficosComponent,
+    PrincipalComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/graficos/graficos.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GraficosComponent } from './graficos.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [GraficosComponent]
+})
+export class GraficosModule { }
